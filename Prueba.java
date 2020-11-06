040b02d3c4b1261d5760d887e83bd6d08cbd6688
Patricia <p.santamartamedina@gmail.com>
2020-11-06 13:47:54 +0100

    Añado ciudad de residencia y sistema operativo

--- Prueba.java
+++ Prueba.java
@@ -2,6 +2,7 @@ class Prueba{
 	public static void main(String[] args){
 		System.out.println("Patricia Santamarta");
 		System.out.println("Leon. 1987");
+		System.out.println("Leon. Windows");
 		
 	}
 }
